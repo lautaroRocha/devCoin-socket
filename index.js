8fa8546ef9504fb2b9c990c9f5ebd7fc76d78f20
Lautaro Rocha <lautarovrocha@gmail.com>
2023-02-01 10:02:25 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express();
+const port = 7070
+const { createServer } = require("http");
+const { Server } = require("socket.io");
+const httpServer = createServer(app);
+
+const io = new Server(httpServer,  {
+    cors: {
+        origin: ["http://127.0.0.1:5173/", "http://localhost:5173", "https://dev-coin.web.app"]
+    }});
+
+    io.on('connection', (socket) => {
+        console.log(`⚡: ${socket.id} user just connected!`);
+        socket.on('transfer', (socket) => {
+            io.emit('new-transfer', JSON.stringify(socket))
+        });
+
+        ;
+    });
+
+httpServer.listen(port);
+
